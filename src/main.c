adee4b91a1e44c20a68e511ad65bbc989884318f
Johnathan <johnathan99j@yahoo.com>
2015-11-28 18:11:02 +0000

    Fullscreen

--- src/main.c
+++ src/main.c
@@ -5,7 +5,6 @@ static TextLayer *s_output_layer;
 
 static void up_click_handler(ClickRecognizerRef recognizer, void *context) {
   text_layer_set_text(s_output_layer, "Up pressed!");
-  window_set_background_color(s_main_window, GColorBlack);
 }
 
 static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
@@ -14,9 +13,7 @@ static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
 }
 
 static void down_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(s_output_layer, "Down pressed!");
-  window_set_background_color(s_main_window, GColorWhite);
-  
+  text_layer_set_text(s_output_layer, "Down pressed!");  
 }
 
 static void click_config_provider(void *context) {
@@ -29,7 +26,6 @@ static void click_config_provider(void *context) {
 static void main_window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
   GRect window_bounds = layer_get_bounds(window_layer);
-
   // Create output TextLayer
   s_output_layer = text_layer_create(GRect(5, 0, window_bounds.size.w - 5, window_bounds.size.h));
   text_layer_set_font(s_output_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24));
@@ -46,6 +42,9 @@ static void main_window_unload(Window *window) {
 static void init() {
   // Create main Window
   s_main_window = window_create();
+  
+  window_set_fullscreen(s_main_window, true);
+  
   window_set_window_handlers(s_main_window, (WindowHandlers) {
     .load = main_window_load,
     .unload = main_window_unload
